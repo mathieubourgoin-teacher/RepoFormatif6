f16732be20fb6246549713e37f2643570880fe48
geoffreyAmegan <geoffrey095012@gmail.com>
2026-03-05 13:19:47 -0500

    Poisson Clown
    Le poisson clown mange

--- Formatif_S6/src/ameganGeoffrey/aquarium/Piranha.java
+++ Formatif_S6/src/ameganGeoffrey/aquarium/Piranha.java
@@ -1,4 +1,4 @@
-package etudiant03.aquarium;
+package ameganGeoffrey.aquarium;
 
 public class Piranha extends Poisson {
 
--- Formatif_S6/src/ameganGeoffrey/aquarium/PoissonClown.java
+++ Formatif_S6/src/ameganGeoffrey/aquarium/PoissonClown.java
@@ -1,4 +1,4 @@
-package etudiant03.aquarium;
+package ameganGeoffrey.aquarium;
 
 public class PoissonClown extends Poisson {
 
@@ -8,6 +8,6 @@ public class PoissonClown extends Poisson {
 
     @Override
     public void manger() {
-        System.out.println(nom + " mange des frites au ketchup");
+        System.out.println(nom + " mange des petits algues");
     }
 }
