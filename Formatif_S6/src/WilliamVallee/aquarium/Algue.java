54101423482943427aed511c4dc026a79268f8ba
WilliamValleeGit <1141775@cegeplimoilou.ca>
2026-03-19 16:06:39 -0400

    Creation de la Classe Algue
    Creation de la classe Algue & methode de toString()

---
+++ Formatif_S6/src/WilliamVallee/aquarium/Algue.java
@@ -0,0 +1,15 @@
+package WilliamVallee.aquarium;
+
+/**
+ * Formatif_S6
+ *
+ * @author willi
+ * @since 2026-03-19
+ */
+public class Algue {
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
